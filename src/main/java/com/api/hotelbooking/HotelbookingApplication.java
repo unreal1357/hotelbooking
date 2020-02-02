e4aa170fba200d0550cf8dbd1c98de5e036e8970
Jon <jonlight1357@hotmail.co.uk>
2020-02-02 14:36:07 +0000

    commit 03 - database setup

--- src/main/java/com/api/hotelbooking/HotelbookingApplication.java
+++ src/main/java/com/api/hotelbooking/HotelbookingApplication.java
@@ -1,8 +1,9 @@
 package com.api.hotelbooking;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//this will work if i get the sql server running on this machine and use localhost instead
 
 @SpringBootApplication
 public class HotelbookingApplication {
--- src/main/java/com/api/hotelbooking/RoomController.java
+++ src/main/java/com/api/hotelbooking/RoomController.java
@@ -1,16 +1,15 @@
 package com.api.hotelbooking;
 
-import com.api.hotelbooking.Room;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
-@RestController //what does this mean
+@RestController
 public class RoomController {
 
-    @GetMapping("/rooms") //what does get mapping mean
+    @GetMapping("/rooms")
         public List<Room> getAllRooms(){
             return Arrays.asList(new Room(1,100,"standard","2 adults"));
 
